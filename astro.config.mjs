ec187c5e77281305ef18e0246a6fe0b31799a7b5
Krindendo work <lollipopfunkyx9@gmail.com>
2024-03-08 22:43:39 +0100

    feat: implement mdx in project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,18 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({ applyBaseStyles: false }),
     react({ experimentalReactChildren: true }),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "dracula" },
+      remarkRehype: { footnoteLabel: "Footnotes" },
+      gfm: false,
+    }),
   ],
 });
