6e658206bcc9a1887eaeb23a5891faa99a0c06d9
Krindendo work <lollipopfunkyx9@gmail.com>
2024-03-12 23:47:47 +0100

    chore: changed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,10 +13,42 @@ export default defineConfig({
     tailwind({ applyBaseStyles: false }),
     react({ experimentalReactChildren: true }),
     mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: { theme: "dracula" },
-      remarkRehype: { footnoteLabel: "Footnotes" },
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [rehypeSlug],
       gfm: false,
     }),
   ],
 });
+
+/*
+
+        [
+          rehypePrettyCode,
+          {
+            theme: "dark-plus",
+            onVisitLine(node) {
+              // Prevent lines from collapsing in `display: grid` mode, and allow empty
+              // lines to be copy/pasted
+              if (node.children.length === 0) {
+                node.children = [{ type: "text", value: " " }];
+              }
+            },
+            onVisitHighlightedLine(node) {
+              node.properties.className ??= [];
+              node.properties.className.push("line--highlighted");
+            },
+            onVisitHighlightedWord(node) {
+              node.properties.className = ["word--highlighted"];
+            },
+          },
+        ],
+        [
+          rehypeAutolinkHeadings,
+          {
+            properties: {
+              className: ["subheading-anchor"],
+              ariaLabel: "Link to section",
+            },
+          },
+        ],
+*/
